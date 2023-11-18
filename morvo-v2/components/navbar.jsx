785d0b9ef348f9e4f81113171a05ec4eaaaa488e
Luz Alba Posse <luzalbaposse@gmail.com>
2023-11-18 20:58:57 +0300

    zk-verification layout
    this is the layout for zk-verification and some other stuff
    
    Co-Authored-By: Gaga <124353085+rhythmic1234@users.noreply.github.com>

--- morvo-v2/components/navbar.jsx
+++ morvo-v2/components/navbar.jsx
@@ -8,7 +8,7 @@ const Navbar = () => {
   const router = useRouter();
 
   return (
-    <div className="flex flex-1 px-5 py-[17px] bg-orange-50 rounded-lg border border-neutral-700 justify-between items-center inline-flex">
+    <div className="flex flex-1 w-full px-5 py-[17px] bg-orange-50 rounded-lg border border-neutral-700 justify-between items-center inline-flex">
       <div className=" items-center gap-[90px] flex">
         <Link href="/" className={`text-center text-red-500 text-xl ${router.pathname === '/' ? 'font-bold' : 'font-light'} font-poppins tracking-tighter`}>
           home
